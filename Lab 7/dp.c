e9b693d6697bf50d2ccfb53fc228702e270cac93
Nikhil M <nikhilm.cs24@bmsce.ac.in>
2026-04-29 14:22:38 +0530

    Add files via upload

---
+++ Lab 7/dp.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define N 5
+
+sem_t s[N];  
+
+// philosopher function
+void *p(void *arg) {
+    int id = *(int *)arg;
+
+    printf("P%d thinking\n", id);
+    sleep(1);
+
+    sem_wait(&s[id]);              
+    sem_wait(&s[(id + 1) % N]);    
+
+    printf("P%d eating\n", id);
+    sleep(1);
+
+    sem_post(&s[id]);           
+    sem_post(&s[(id + 1) % N]);     
+
+    printf("P%d done\n", id);
+    return NULL;
+}
+
+int main() {
+    pthread_t t[N];
+    int id[N];
+
+    for (int i = 0; i < N; i++)
+        sem_init(&s[i], 0, 1);
+
+    for (int i = 0; i < N; i++) {
+        id[i] = i;
+        pthread_create(&t[i], NULL, p, &id[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+        pthread_join(t[i], NULL);
+
+    return 0;
+}
---
+++ Lab 7/pc.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define N 5
+
+int b[N];
+int i = 0, o = 0;
+
+sem_t e, f;
+pthread_mutex_t m;
+
+// producer
+void *p(void *arg) {
+    int x;
+    for (int k = 0; k < 10; k++) {
+        x = rand() % 100;
+
+        sem_wait(&e);
+        pthread_mutex_lock(&m);
+
+        b[i] = x;
+        printf("P:%d @%d\n", x, i);
+        i = (i + 1) % N;
+
+        pthread_mutex_unlock(&m);
+        sem_post(&f);
+    }
+    return NULL;
+}
+
+// consumer
+void *c(void *arg) {
+    int x;
+    for (int k = 0; k < 10; k++) {
+        sem_wait(&f);
+        pthread_mutex_lock(&m);
+
+        x = b[o];
+        printf("C:%d @%d\n", x, o);
+        o = (o + 1) % N;
+
+        pthread_mutex_unlock(&m);
+        sem_post(&e);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    sem_init(&e, 0, N);
+    sem_init(&f, 0, 0);
+    pthread_mutex_init(&m, NULL);
+
+    pthread_create(&t1, NULL, p, NULL);
+    pthread_create(&t2, NULL, c, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&e);
+    sem_destroy(&f);
+    pthread_mutex_destroy(&m);
+
+    return 0;
+}
